859de428d04c002de7bfcdb227d5e373938e5b75
qizixi <alexqizixi@gmail.com>
2019-03-11 10:42:21 -0400

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className='App'>
+	      <div className='app-container'>
+
+	        <div className='side-bar'>
+	          sidebar
+	        </div>
+
+	        <div className='main-section'>
+	          <div className='main-section-header' >
+              Spot A Song
+            </div>
+	          <div className='main-section-container'>
+              Main Section
+	          </div>
+	        </div>
+	      </div>
+	    </div>
     );
   }
 }
