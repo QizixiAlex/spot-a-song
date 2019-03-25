4cda3be7ef51309c0a3540f2640c0c623414b56c
qizixi <alexqizixi@gmail.com>
2019-03-25 10:02:12 -0400

    3-25

--- backend/server/server.js
+++ backend/server/server.js
@@ -8,17 +8,40 @@ app.use(cors());
 const connection = mysql.createConnection({
   host:'localhost',
   user:'root',
-  password:'rootpassword',
+  password:'password',
   database:'dev0'
 });
 connection.connect(err => {
   if (err) {
-    return err;
+    console.log(err);
+  } else {
+    console.log('connection success!');
   }
 });
 
 app.get('/', (req, res)=>{
+  // const {name, age} = req.query;
+  // console.log(`name->${name}`);
+  // console.log(`age->${age}`);
   res.send('welcome to dev server!');
+})
+
+app.post('/user/login', (req, res)=>{
+  const {userName, userPassword} = req.query;
+  // TODO:
+})
+
+app.post('/user/register', (req, res)=>{
+  const {userName, userEmail, userPassword} = req.query;
+  let query = `SELECT COUNT(user_id) FROM users WHERE user_name = "${userName}"`;
+  console.log(query);
+  connection.query(query, (err, results)=> {
+    if (err) {
+      return res.send(err);
+    } else {
+      console.log(results);
+    }
+  })
 })
 
 app.listen(4000, ()=>{
